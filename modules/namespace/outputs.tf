b6559d3b25d81e18041de8b577d08b2e42594b56
Byungjin Park <posquit0.bj@gmail.com>
2025-09-10 23:51:55 +0900

    feat: improve how to use resource_group in modules

--- modules/namespace/outputs.tf
+++ modules/namespace/outputs.tf
@@ -17,3 +17,19 @@ output "capacity_region" {
   description = "The AWS Region that you want to use for the free SPICE capacity for the new namespace."
   value       = aws_quicksight_namespace.this.capacity_region
 }
+
+output "resource_group" {
+  description = "The resource group created to manage resources in this module."
+  value = merge(
+    {
+      enabled = var.resource_group.enabled && var.module_tags_enabled
+    },
+    (var.resource_group.enabled && var.module_tags_enabled
+      ? {
+        arn  = module.resource_group[0].arn
+        name = module.resource_group[0].name
+      }
+      : {}
+    )
+  )
+}
